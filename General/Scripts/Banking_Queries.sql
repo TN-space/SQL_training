1b09afe1ac3db23c40ca308d21f0fe822fcab114
Thanh Nguyen <tnansci@gmail.com>
2022-02-14 20:45:33 -0500

    Added BankingDB for future reference. Added questions for Simple_Queries and Banking_Queries.

--- General/Scripts/Banking_Queries.sql
+++ General/Scripts/Banking_Queries.sql
@@ -1,10 +1,11 @@
 use Banking;
 
--- 1
+/*1. For each product, show the product name "Product" and the product type name "Type".*/
 SELECT NAME as `Product`, PRODUCT_TYPE_CD as `Type`
 from product;
 
--- 2
+/*2. For each branch, list the branch name and city, 
+ *plus the last name and title of each employee who works in that branch.*/
 -- SELECT * from branch as b ;
 -- SELECT * from employee as e;
 SELECT b.name as `Name`, b.city as `City`, e.last_name as `Last Name`, e.title as `Title` 
@@ -12,24 +13,26 @@ from branch as b
 join employee as e
 on b.BRANCH_ID = e.ASSIGNED_BRANCH_ID;
 
--- 3
+-- 3. Show a list of each unique employee title.
 SELECT DISTINCT title as `Title` from employee;
 
--- 4
+/*4. Show the last name and title of each employee, 
+ *along with the last name and title of that employee's boss.*/
 -- SELECT * from employee as e;
 SELECT e.last_name as `Last Name`, e.title as `Title`, s.LAST_NAME as `Supervisor Last Name`, s.title as `Supervisor Title` 
 from employee as e
 left join employee as s
 on e.SUPERIOR_EMP_ID = s.EMP_ID;
 
--- 5
+/*5. For each account, show the name of the account's product,
+ *the available balance, and the customer's last name.*/
 -- SELECT * from individual i ;
 SELECT p.NAME as `Name`, a.avail_balance as `Available Balance`, i.last_name as `Customer Last Name`
 from account as a
 join product as p on p.PRODUCT_CD = a.product_CD 
 join individual as i on a.CUST_ID = i.CUST_ID;
 
--- 6 
+/*6. List all account transaction details for individual customers whose last name starts with 'T'.*/
 -- SELECT * from acc_transaction at2 ;
 SELECT atr.*
 FROM acc_transaction as atr
--- General/Scripts/Simple_Queries.sql
+++ General/Scripts/Simple_Queries.sql
@@ -1,35 +1,45 @@
 use classicmodels;
 
--- 1
+/* 1. Write a query to display the name, product line, and buy price of all products.
+ * The output columns should display as “Name”, “Product Line”, and “Buy Price”.
+ * The output should display the most expensive items first.*/
 SELECT productName as `Name`, productLine as `Product Line`, buyPrice as `Buy Price`
 from products
 Order by buyPrice desc;
 
--- 2
+/*2. Write a query to display the first name, last name, and city for all customers from Germany. 
+ *Columns should display as “First Name”, “Last Name”, and “City”.
+ *The output should be sorted by the customer’s last name (ascending).*/
 SELECT contactFirstName as `First Name`, contactLastName as `Last Name`, city as `City`
 from customers
 WHERE country = 'Germany'
 ORDER by contactLastName ASC;
 
--- 3
+/*3. Write a query to display each of the unique values of the status field in the orders table.
+ *The output should be sorted alphabetically increasing.
+ *Hint: the output should show exactly 6 rows.*/
 SELECT DISTINCT status 
 from orders
 order by status ASC;
 
--- 4
+/*4. Select all fields from the payments table for payments made on or after January 1, 2005.
+ *Output should be sorted by increasing payment date.*/
 SELECT * 
 FROM payments
 WHERE paymentDate >= '2005-01-01'
 ORDER by paymentDate asc;
 
--- 5
+/*5. Write a query to display all Last Name, First Name, Email and Job Title of all employees working out of the San Francisco office.
+ *Output should be sorted by last name.*/
 -- SELECT * FROM offices ;
 SELECT lastName as `Last Name`, firstName as `First Name`, email as `Email`, jobTitle as `Job Title`
 FROM employees
 WHERE officeCode = 1
 ORDER by lastName ASC;
 
--- 6
+/*6. Write a query to display the Name, Product Line, Scale, and Vendor of all of the car products – both classic and vintage.
+ *The output should display all vintage cars first (sorted alphabetically by name),
+ *and all classic cars last (also sorted alphabetically by name).*/
 SELECT SUBSTRING(productName, 6) as `Name`, productLine as `Product Line`, productScale as `Scale`, productVendor as `Vendor` 
 FROM products
 WHERE productLine = 'classic cars' or productLine = 'Vintage Cars'
