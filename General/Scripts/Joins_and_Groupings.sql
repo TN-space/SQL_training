fefc5f975a896e48dc978bd521281724558b35bb
Thanh Nguyen <tnansci@gmail.com>
2022-02-15 18:03:25 -0500

    Completed the exercise.

--- General/Scripts/Joins_and_Groupings.sql
+++ General/Scripts/Joins_and_Groupings.sql
@@ -34,3 +34,47 @@ SELECT status as `Order Status`, count(*) as `# Orders`
 from orders
 GROUP by status 
 ORDER by `Order Status` ASC;
+
+/*4. Write a query to list, for each product line, the total # of products sold from that product line.
+ *The first column should be “Product Line” and the second should be “# Sold”.\
+ *Order by the second column descending.*/
+-- SELECT * from productlines p ;
+-- SELECT * from products p2 ;
+-- SELECT * from orderdetails o ;
+SELECT p.productLine as `Product Line`, sum(quantityOrdered) as `# Sold`
+FROM products as p 
+join orderdetails as od on p.productCode = od.productCode 
+GROUP by `Product Line`
+ORDER by `# Sold` desc;
+
+/*5. For each employee who represents customers, output the total # of orders that employee’s customers have placed
+ *alongside the total sale amount of those orders. The employee name should be output as a single column named “Sales Rep”
+ *formatted as “lastName, firstName”. The second column should be titled “# Orders” and the third should be “Total Sales”.
+ *Sort the output by Total Sales descending. Only (and all) employees with the job title ‘Sales Rep’ should be included in the output, 
+ *and if the employee made no sales the Total Sales should display as “0.00”.*/
+-- SELECT * from customers c ;
+-- SELECT * from employees e ;
+-- SELECT * from orders o ;
+-- SELECT * from orderdetails o ;
+SELECT concat(e.lastName,', ',e.firstName) as `Sales Rep`, if(od.quantityOrdered is null, '0.00', sum(od.quantityOrdered)) as `# Orders`, if(od.quantityOrdered is null, '0.00', sum(od.quantityOrdered) * od.priceEach) as `Total Sales` -- , c.customerNumber, c.customerNumber, o.orderNumber,  
+FROM employees e 
+left join customers c on e.employeeNumber = c.salesRepEmployeeNumber
+left join orders o on  c.customerNumber = o.customerNumber 
+left join orderdetails od on o.orderNumber = od.orderNumber
+where e.jobTitle = 'Sales Rep'
+GROUP by e.employeeNumber  
+ORDER by `Total Sales` desc;
+
+/*6. Your product team is requesting data to help them create a bar-chart of monthly sales since the company’s inception. 
+ *Write a query to output the month (January, February, etc.), 4-digit year, and total sales for that month. 
+ *The first column should be labeled ‘Month’, the second ‘Year’, and the third should be ‘Payments Received’. 
+ *Values in the third column should be formatted as numbers with two decimals – for example: 694,292.68.*/
+-- SELECT * from payments p order by year(paymentDate), month(paymentDate);
+SELECT monthname(paymentDate) as `Month`, year(paymentDate) as `Year`, FORMAT(sum(amount), 2) as `Payments Received`  
+from payments p
+group by year(paymentDate), month(paymentDate)
+order by year(paymentDate), month(paymentDate);
+
+
+
+
