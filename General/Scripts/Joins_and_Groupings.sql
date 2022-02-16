098a5e5fe7453924f94a8626642bed991dd3d99b
Thanh Nguyen <tnansci@gmail.com>
2022-02-16 16:46:25 -0500

    Added Joins and Groupings with fixed solutions.

--- General/Scripts/Joins_and_Groupings.sql
+++ General/Scripts/Joins_and_Groupings.sql
@@ -68,7 +68,7 @@ ORDER by `Total Sales` desc;
 /*6. Your product team is requesting data to help them create a bar-chart of monthly sales since the company’s inception. 
  *Write a query to output the month (January, February, etc.), 4-digit year, and total sales for that month. 
  *The first column should be labeled ‘Month’, the second ‘Year’, and the third should be ‘Payments Received’. 
- *Values in the third column should be formatted as numbers with two decimals – for example: 694,292.68.*/
+ *Values in the third column should be formatted as numbers with 	two decimals – for example: 694,292.68.*/
 -- SELECT * from payments p order by year(paymentDate), month(paymentDate);
 SELECT monthname(paymentDate) as `Month`, year(paymentDate) as `Year`, FORMAT(sum(amount), 2) as `Payments Received`  
 from payments p
